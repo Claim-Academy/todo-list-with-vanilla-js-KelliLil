eb7724f8baf2d1ffb64298e987b83e1da054829e
Kelli Lilly <kelli.lilly05@gmail.com>
2022-12-14 13:51:08 -0600

    Add function

--- src/index.js
+++ src/index.js
@@ -3,3 +3,15 @@ const root = document.querySelector("#root");
 const todo = await fetch("https://jsonplaceholder.typicode.com/todos");
 
 const toDos = await todo.json();
+function createIdCard(user) {
+    return `
+    <section>
+        <h2> UserID: ${user.userId} </h2>
+        <ul>
+            <li> ID Number: ${user.id}<li>
+            <li> Title of User: ${user.title}<li>
+            <li> Task Completed: ${user.completed}<li>
+        </ul>
+    </section>
+        `;
+}
