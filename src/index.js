c5a3ab2f3334681a7e62331342efef55331af96e
Kelli Lilly <kelli.lilly05@gmail.com>
2022-12-16 09:38:32 -0600

    Change code and insert checkbox for tasks

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 const root = document.querySelector("#root");
 
-let letters = "";
 
-function createIdCard(user) {
+function createToDoLI(todo) {
     return `
     <section class="bg-gradient-to-r from-purple-500 to-pink-500 text-white p-9 ">
-    <h2> UserID: ${user.userId} </h2>
+    <h2> UserID: ${todo.userId} </h2>
     <ul>
-        <li> ID Number: ${user.id}<li>
-        <li> Title of User: ${user.title}<li>
-        <li> Task Completes: ${user.completed}<li>
+        <li> ID Number: ${todo.id}<li>
+        <li> Title of User: ${todo.title}<li>
+        <li> Task Completes: ${todo.completed}<li>
     </ul>
 </section>
 `;
@@ -21,28 +20,25 @@ const toDos = await resp.json();
 root.classList.add("flex", "flex-col", "gap-y-8", "items-center")
 
 
-const allIdCard = toDos.map(createIdCard).join(" ");
+const todoLIs = toDos.map(createToDoLI).join();
 
  root.innerHTML = `
  <div class="shadow-lg p-1 mx-auto">
-    <label for="search" class="sr-only">Search</label>
-    <input type="search" id="search" placeholder="Search" />
+    <label for="completed">Show Completed</label>
+    <input type="checkbox" id="completed"/>
  </div>
  <main class = "items-center grid grid-cols-4 gap-2">
-${allIdCard}
+${todoLIs}
  </main>`
 
- function filterByComplete(idList) {
+ function filterByComplete(todoList) {
 
-    return allIdCard.filter((task) => task.completed.includes(idList))
+    return todoList.filter((task) => task.completed)
  }
 
- const search = document.querySelector("input")
+ const checkbox = document.querySelector("input")
 
-search.addEventListener("keydown" , (event) => {
-letters += event.key;
-const filterId = filterByComplete(letters)
-
-console.log(filterId)
-});
+checkbox.addEventListener("change", () => {
+    console.log(filterByComplete(toDos))
+})
 
